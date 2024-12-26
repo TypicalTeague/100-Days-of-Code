05b99b999bddee437bccdf2553d280699f3c9767
TypicalTeague <140266575+TypicalTeague@users.noreply.github.com>
2024-12-25 22:42:03 -0500

    Add files via upload
    Day 1 of my 100 days of code Journey

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello Welcome to Band Name Generator!!")
+city = input("What city did you grow up in?\n")
+petName = input("What is the name of your pet?\n")
+print(f"Your Band Name is {city} {petName}.")
